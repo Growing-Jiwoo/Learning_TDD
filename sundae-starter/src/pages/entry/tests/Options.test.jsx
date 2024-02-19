994f6ce9270a2525e1f15c6bce5004306f353bc6
Growing-Jiwoo <fbaa4412@naver.com>
2024-02-19 16:39:07 +0900

    추가 과제 4 풀이

--- sundae-starter/src/pages/entry/tests/Options.test.jsx
+++ sundae-starter/src/pages/entry/tests/Options.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from "../../../test-utils/testing-library-utils";
+import { userEvent } from "@testing-library/user-event";
 import Options from "../Options";
 
 test("displays image for each scoop option from server", async () => {
@@ -30,4 +31,32 @@ test("Displays image for each toppings option from server", async () => {
     "M&Ms topping",
     "Hot fudge topping",
   ]);
+});
+
+// 유효하지 않은 스쿱 개수가 있으면 합계액을 업데이트 하지 않도록
+// -> 스쿱 갯수 값 유효성 검사 이후, 최소한의 렌더링, userEvent keyboard 메소드 사용
+
+test("유효하지 않은 스쿱 개수가 있으면 합계액을 업데이트 하지 않음", async () => {
+  const user = userEvent.setup();
+
+  render(<Options optionType="scoops" />);
+
+  const chocolateScoop = await screen.findByRole("spinbutton", {
+    name: "Chocolate",
+  });
+
+  await user.clear(chocolateScoop);
+  await user.type(chocolateScoop, "1.5");
+
+  screen.getByText("Scoops total: $0.00");
+
+  await user.clear(chocolateScoop);
+  await user.type(chocolateScoop, "-5");
+
+  screen.getByText("Scoops total: $0.00");
+
+  await user.clear(chocolateScoop);
+  await user.type(chocolateScoop, "1");
+
+  screen.getByText("Scoops total: $2.00");
 });
